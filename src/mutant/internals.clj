be66ecdb6a4375812a48ccc49e3a2c57b471803c
Jan Stępień <jstepien@users.sourceforge.net>
2016-05-17 19:20:25 +0200

    Extract mutations to a dedicated ns

--- src/mutant/internals.clj
+++ src/mutant/internals.clj
@@ -1,5 +1,6 @@
 (ns mutant.internals
   (:require [rewrite-clj.zip :as z]
+            [mutant.mutations :refer [mutate]]
             [clojure.tools.namespace
              [find :as find]
              [file :as file]
@@ -15,65 +16,6 @@
                                 (rec (conj prefix dir) sub-node))))
                       directions))]
     (cons [] (rec [] zipper))))
-
-(defn- swapping-mutation [from to]
-  (fn [node]
-    (condp = (z/sexpr node)
-      to   [(z/replace node from)]
-      from [(z/replace node to)]
-      nil)))
-
-(def ^:private and-or
-  (swapping-mutation 'and 'or))
-
-(def ^:private not-boolean
-  (swapping-mutation 'boolean 'not))
-
-(def ^:private empty?-seq
-  (swapping-mutation 'empty? 'seq))
-
-(def ^:private gt-gte
-  (swapping-mutation '<= '<))
-
-(def ^:private lt-lte
-  (swapping-mutation '>= '>))
-
-(def ^:private eq-noteq
-  (swapping-mutation '= 'not=))
-
-(defn- for->doseq [node]
-  (if (= 'for (z/sexpr node))
-    [(z/replace node 'doseq)]))
-
-(defn- rm-args [node]
-  (let [sexpr (z/sexpr node)]
-    (if (seq? sexpr)
-      (let [[defn name args & more] sexpr]
-        (if (and (#{'defn 'defn-} defn)
-                 (vector? args))
-          (for [arg args]
-            (-> node z/down z/right z/right
-                (z/edit (partial filterv (complement #{arg})))
-                (z/up))))))))
-
-(def ^:private mutations
-  [and-or
-   gt-gte
-   lt-lte
-   rm-args
-   eq-noteq
-   empty?-seq
-   for->doseq
-   not-boolean])
-
-(defn- mutate [sexpr]
-  (->> mutations
-       (mapcat (fn [m]
-                 (try
-                   (m sexpr)
-                   (catch UnsupportedOperationException ex
-                     nil))))
-       (remove nil?)))
 
 (defn- mutants [zipper paths]
   (->> (for [path paths]
---
+++ src/mutant/mutations.clj
@@ -0,0 +1,61 @@
+(ns mutant.mutations
+  (:require [rewrite-clj.zip :as z]))
+
+(defn- swapping-mutation [from to]
+  (fn [node]
+    (condp = (z/sexpr node)
+      to   [(z/replace node from)]
+      from [(z/replace node to)]
+      nil)))
+
+(def ^:private and-or
+  (swapping-mutation 'and 'or))
+
+(def ^:private not-boolean
+  (swapping-mutation 'boolean 'not))
+
+(def ^:private empty?-seq
+  (swapping-mutation 'empty? 'seq))
+
+(def ^:private gt-gte
+  (swapping-mutation '<= '<))
+
+(def ^:private lt-lte
+  (swapping-mutation '>= '>))
+
+(def ^:private eq-noteq
+  (swapping-mutation '= 'not=))
+
+(defn- for->doseq [node]
+  (if (= 'for (z/sexpr node))
+    [(z/replace node 'doseq)]))
+
+(defn- rm-args [node]
+  (let [sexpr (z/sexpr node)]
+    (if (seq? sexpr)
+      (let [[defn name args & more] sexpr]
+        (if (and (#{'defn 'defn-} defn)
+                 (vector? args))
+          (for [arg args]
+            (-> node z/down z/right z/right
+                (z/edit (partial filterv (complement #{arg})))
+                (z/up))))))))
+
+(def ^:private mutations
+  [and-or
+   gt-gte
+   lt-lte
+   rm-args
+   eq-noteq
+   empty?-seq
+   for->doseq
+   not-boolean])
+
+(defn mutate [zipper]
+  (->> mutations
+       (mapcat (fn [m]
+                 (try
+                   (m zipper)
+                   (catch UnsupportedOperationException ex
+                     nil))))
+       (remove nil?)))
