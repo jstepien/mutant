13ca063f7fe4ce8ecd5615d66315abe4b6d99281
Jan Stępień <jan.stepien@innoq.com>
2017-02-18 17:44:13 +0100

    Juggle the dependencies vector

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject mutant "0.1.0"
+(defproject mutant "0.2.0-SNAPSHOT"
   :description "Mutation testing for Clojure"
   :url "https://github.com/jstepien/mutant"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [clj-diffmatchpatch "0.0.9.3"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [clj-diffmatchpatch "0.0.9.3" :exclusions [org.clojure/clojure]]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [jansi-clj "0.1.0"]
-                 [rewrite-clj "0.5.0"]])
+                 [jansi-clj "0.1.0" :exclusions [org.clojure/clojure]]
+                 [rewrite-clj "0.6.0"]])
